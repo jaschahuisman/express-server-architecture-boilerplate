d043cf2f51fe1e4fbf611b523452f0b1282b2894
Jascha Huisman <jascha.huisman@gmail.com>
2021-08-03 19:42:20 +0200

    New build

--- build/index.js
+++ build/index.js
@@ -3,6 +3,254 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/api/example/example.controller.ts":
+/*!***********************************************!*\
+  !*** ./src/api/example/example.controller.ts ***!
+  \***********************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.checkOwnership = exports.getByName = exports.createExampleDocument = void 0;
+var example_model_1 = __importDefault(__webpack_require__(/*! ./example.model */ "./src/api/example/example.model.ts"));
+/**
+ * ### createExampleDocument
+ * Create a new example document from the request body's JSON data.
+ * @param  {express.Request} req
+ * @param  {express.Response} res
+ * @param  {express.NextFunction} next
+ * @returns {void}
+ */
+function createExampleDocument(req, res, next) {
+    var exampleFromBody = req.body;
+    var example = new example_model_1.default(exampleFromBody);
+    example.save(function (err) {
+        if (err)
+            return next(err);
+        res.json(example);
+    });
+}
+exports.createExampleDocument = createExampleDocument;
+/**
+ * ### getByName
+ * Get a example document by its name.
+ * @param  {express.Request} req
+ * @param  {express.Response} res
+ * @param  {express.NextFunction} next
+ * @returns {Promise<void>}
+ */
+function getByName(req, res, next) {
+    return __awaiter(this, void 0, void 0, function () {
+        var query, example;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    query = req.body.name;
+                    return [4 /*yield*/, example_model_1.default.getByName(query)];
+                case 1:
+                    example = _a.sent();
+                    if (!example)
+                        return [2 /*return*/, next(new Error('Example document not found'))];
+                    res.json(example);
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+exports.getByName = getByName;
+/**
+ * ### checkOwnership
+ * Check a example document's ownership by its name & owner.
+ * @param  {express.Request} req
+ * @param  {express.Response} res
+ * @param  {express.NextFunction} next
+ * @returns {Promise<void>}
+ */
+function checkOwnership(req, res, next) {
+    return __awaiter(this, void 0, void 0, function () {
+        var query, owner, example, isOwner;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    query = req.body.name;
+                    owner = req.body.owner;
+                    return [4 /*yield*/, example_model_1.default.getByName(query)];
+                case 1:
+                    example = _a.sent();
+                    if (!example)
+                        return [2 /*return*/, next(new Error('Example not found'))];
+                    isOwner = example.checkOwnership(owner);
+                    res.json(isOwner);
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+exports.checkOwnership = checkOwnership;
+
+
+/***/ }),
+
+/***/ "./src/api/example/example.model.ts":
+/*!******************************************!*\
+  !*** ./src/api/example/example.model.ts ***!
+  \******************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+var mongoose_1 = __importDefault(__webpack_require__(/*! mongoose */ "mongoose"));
+var exampleSchema = new mongoose_1.default.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    created: { type: Date, default: Date.now },
+    modified: { type: Date, default: Date.now },
+    owner: { type: String, required: true },
+    tags: { type: [String], default: [] },
+    content: { type: String, default: '' },
+});
+exampleSchema.methods = {
+    /**
+     * ### checkOwnership
+     * Example method that checks if the owner of a specified \
+     * example document is the same as the given owner parameter.
+     * @param {string} owner
+     * @returns {boolean}
+     */
+    checkOwnership: function (owner) {
+        return this.owner === owner;
+    },
+};
+exampleSchema.statics = {
+    /**
+     * ### getByName
+     * Example static method that gets a single document by name.
+     * @param  {string} name
+     * @returns {Promise<ExampleDocument | undefined>}
+     */
+    getByName: function (name) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.findOne({ name: name }).exec()];
+                    case 1: return [2 /*return*/, _a.sent()];
+                }
+            });
+        });
+    },
+};
+/**
+ * ## exampleModel
+ * Mongoose model that implements the exampleSchema & IExampleModel interface.
+ * @extends {ExampleDocument, IExampleModel}
+ */
+var exampleModel = mongoose_1.default.model('Example', exampleSchema);
+exports.default = exampleModel;
+
+
+/***/ }),
+
+/***/ "./src/api/example/example.router.ts":
+/*!*******************************************!*\
+  !*** ./src/api/example/example.router.ts ***!
+  \*******************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+var express_1 = __importDefault(__webpack_require__(/*! express */ "express"));
+var example_controller_1 = __webpack_require__(/*! ./example.controller */ "./src/api/example/example.controller.ts");
+var exampleRouter = express_1.default.Router();
+exampleRouter.get('/', function (req, res) { return res.send('Hello from Example Router!'); });
+exampleRouter.get('/checkOwner', example_controller_1.checkOwnership);
+exampleRouter.get('/getByName', example_controller_1.getByName);
+exampleRouter.post('/create', example_controller_1.createExampleDocument);
+exports.default = exampleRouter;
+
+
+/***/ }),
+
 /***/ "./src/common/database.ts":
 /*!********************************!*\
   !*** ./src/common/database.ts ***!
@@ -253,10 +501,14 @@ database.connect();
 /*!***********************!*\
   !*** ./src/routes.ts ***!
   \***********************/
-/***/ ((__unused_webpack_module, exports) => {
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
 
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
+var example_router_1 = __importDefault(__webpack_require__(/*! ./api/example/example.router */ "./src/api/example/example.router.ts"));
 /**
  * ## Routes
  * Configure the custom routes for the api application by \
@@ -273,7 +525,7 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
  * @param  {express.Application} app
  */
 function routes(app) {
-    app.use('/', function () { });
+    app.use('/example', example_router_1.default);
 }
 exports.default = routes;
 
